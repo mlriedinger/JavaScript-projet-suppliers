c3679156db9f9096bb67e1e7731e76a00545a7d2
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-04-08 13:57:03 +0200

    Ajout du composant Supplier
    Etape 5.1 : OK

---
+++ src/components/Supplier.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <h3>Fournisseur : {{ name }}</h3>
+        <h5 v-if="status" style="color:#339966">En stock</h5>
+        <h5 v-else style="color:red">Rupture de stock</h5>
+        <h6>Stock relevé le : {{ checkedAt }}</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Supplier',
+    data: () => {
+        return {
+            name: 'George de la Jungle',
+            status: false, // est ce qu'il y a du stock
+            checkedAt: new Date().toLocaleString('fr-FR'), // date de la dernière mise à jour du stock
+        };
+    },
+};
+</script>
+
+<style scoped>
+.container {
+    box-shadow: 5px 5px 5px #999999;
+    border-radius: 1em;
+    margin-top: 2em;
+    padding: 1em;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import SuppliersList from '../components/SuppliersList.vue';
 import SuppliersMap from '../components/SuppliersMap.vue';
+import Supplier from '../components/Supplier.vue';
 
 Vue.use(VueRouter);
 
@@ -20,6 +21,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         // component: () =>
         //     import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+    {
+        path: '/supplier',
+        name: 'Supplier',
+        component: Supplier,
     },
 ];
 
