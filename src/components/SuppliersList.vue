0afbdfea5842cd84a3856133b1eb467965b747bc
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-04-27 15:24:33 +0200

    Etape 8.3 : OK
    Ajout d'une requête vers une API avec axios pour alimenter la liste de fournisseurs (utilisation de promises)

--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
         <h3>Liste des fournisseurs</h3>
+        <div v-if="error!=null">{{ error }}</div>
+        <div v-if="loading">Requête en cours ...</div>
         <Supplier
             v-for="supplier in suppliers"
             :key="supplier.id"
@@ -13,6 +15,7 @@
 
 <script>
 import Supplier from './Supplier.vue';
+const axios = require('axios').default;
 
 export default {
     name: 'SuppliersList',
@@ -21,39 +24,22 @@ export default {
     },
     data: () => {
         return {
-            suppliers: [
-                {
-                    id: 1,
-                    name: 'George de la Jungle',
-                    status: true,
-                    checkedAt: new Date().toLocaleString('fr-FR'),
-                },
-                {
-                    id: 2,
-                    name: 'Toutânkhamon',
-                    status: false,
-                    checkedAt: new Date().toLocaleString('fr-FR'),
-                },
-                {
-                    id: 3,
-                    name: 'Simba',
-                    status: true,
-                    checkedAt: new Date().toLocaleString('fr-FR'),
-                },
-                {
-                    id: 4,
-                    name: "Bob l'éponge",
-                    status: true,
-                    checkedAt: new Date().toLocaleString('fr-FR'),
-                },
-                {
-                    id: 5,
-                    name: 'Dark Vador',
-                    status: false,
-                    checkedAt: new Date().toLocaleString('fr-FR'),
-                },
-            ],
+            suppliers: [],
+            loading: false,
+            error: null,
         };
+    },
+    created: function loadSuppliers() {
+        this.loading = true;
+        axios
+            .get('https://api-suppliers.herokuapp.com/api/suppliers')
+            .then(loadedValue => {
+                this.suppliers = loadedValue.data;
+                this.loading = false;
+            })
+            .catch(rejectReason => {
+                this.error = rejectReason;
+            });
     },
 };
 </script>
