90fc5e8d0b99091a6bdafd1f93dbcac2ee2f3c7b
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-04-28 17:25:38 +0200

    Ajout footer + bêtises

--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -3,6 +3,12 @@
         <h3>Liste des fournisseurs</h3>
         <div v-if="error != null">{{ error }}</div>
         <div v-if="loading">Requête en cours ...</div>
+        <div class="text-dark text-justify" style="margin-top:2em;">
+            Voici une liste de fournisseurs .. plutôt douteux. Tout ce que vous pourrez lire par la suite pourrait endommager gravement votre santé mais aussi celle de vos proches (évidemment).
+        </div>
+        <div class="text-dark text-justify" style="margin-top:2em;">
+            Vous êtes toujours là ? <br/>Ye be warned .. Mouahaha (rire du Dr. Denfer)
+        </div>
         <Supplier
             v-for="supplier in suppliers"
             :key="supplier.id"
